08b06da31e62ac36a5411fea141b7927a6f72c85
OuiSouss <toto@tot.o>
2018-10-08 22:05:26 +0200

    td5 : CatalogName begin implementation

---
+++ td/td5/src/main/java/fr/ubordeaux/ao/ui/CommandLineMain.java
@@ -0,0 +1,16 @@
+package fr.ubordeaux.ao.ui;
+
+import java.io.IOException;
+
+public class CommandLineMain {
+
+    public static void main(String[] args) {
+        try {
+            TextualMenu tm = new TextualMenu(System.in, System.out);
+            tm.handleUserInstructions();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ td/td5/src/test/java/fr/ubordeaux/ao/TestAcceptation.java
@@ -0,0 +1,33 @@
+package test.java.fr.ubordeaux.ao;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Set;
+
+import fr.ubordeaux.ao.domain.model.Catalog;
+import fr.ubordeaux.ao.domain.model.Reference;
+import fr.ubordeaux.ao.domain.type.Price;
+import fr.ubordeaux.ao.infrastructure.inmemory.CatalogImpl;
+
+public class TestAcceptation {
+    private Catalog catalog;
+
+    @Before
+    public void createCatalog() {
+        catalog = new CatalogImpl();
+    }
+
+    @Test
+    public void testAddReferenceAndFind() {
+        Price price = new Price(1000);
+        Reference reference = new Reference("ID", "Foo", "Bar", price);
+        catalog.addReference(reference);
+        Set<Reference> references = catalog.getReferences();
+        for (Reference r : references) {
+            assertEquals(r, reference);
+        }
+    }
+}
