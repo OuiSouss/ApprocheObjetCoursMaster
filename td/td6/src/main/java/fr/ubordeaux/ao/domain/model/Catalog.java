898f5df007f63c6f0c049cbf8c5a31ab71f8e5e5
OuiSouss <toto@tot.o>
2018-10-15 17:39:06 +0200

    td6 : Add files to begin td

--- td/td6/src/main/java/fr/ubordeaux/ao/domain/model/Catalog.java
+++ td/td6/src/main/java/fr/ubordeaux/ao/domain/model/Catalog.java
@@ -1,9 +1,13 @@
 package fr.ubordeaux.ao.domain.model;
 
 import java.util.Set;
+import java.util.UUID;
+
+import fr.ubordeaux.ao.domain.type.CatalogName;
 
 public interface Catalog {
     public int size();
+    public CatalogName getCatalogName();
     public Set<Reference> getReferences();
     public Reference findReferenceById(String id);
     public void addReference(Reference reference);
--- td/td6/src/main/java/fr/ubordeaux/ao/domain/service/SearchEngine.java
+++ td/td6/src/main/java/fr/ubordeaux/ao/domain/service/SearchEngine.java
@@ -1,5 +1,44 @@
 package fr.ubordeaux.ao.domain.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import fr.ubordeaux.ao.domain.exception.ReferenceManagementException;
+import fr.ubordeaux.ao.domain.model.Catalog;
+import fr.ubordeaux.ao.domain.model.Reference;
+
 public class SearchEngine {
-    //TODO: Ajouter des méthodes permettant de chercher des références dans le catalogue.
+    private Catalog catalog;
+
+    public SearchEngine(Catalog catalog) {
+        this.setCatalog(catalog);
+    }
+
+    private void setCatalog(Catalog catalog) {
+        if (catalog == null)
+            throw new ReferenceManagementException("cannot create SearchEngine"
+                                                   + "with null as catalog");
+        this.catalog = catalog;
+    }
+
+    public Reference searchReferenceById(String id) {
+        for (Reference reference : catalog.getReferences()) {
+            if (reference.getId().compareTo(id)==0) {
+                return reference;
+            }
+        }
+        throw new ReferenceManagementException("cannot find reference,"
+                                               + "id does not exist");
+    }
+
+    public Set<Reference> searchReferencesByName(String name) {
+        Set<Reference> foundReferences = new HashSet<Reference>();
+        for (Reference reference : catalog.getReferences()) {
+            if (reference.getName().compareTo(name)==0) {
+                foundReferences.add(reference);
+            }
+        }
+        return foundReferences;
+    }
+
 }
