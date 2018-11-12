a6981026ff806fda64bf7ab8bde486ddc564e53c
OuiSouss <toto@tot.o>
2018-11-12 17:34:33 +0100

    td7 : Command in progress

--- td/td7/src/java/fr/ubordeaux/ao/application/command/Command.java
+++ td/td7/src/java/fr/ubordeaux/ao/application/command/Command.java
@@ -8,7 +8,23 @@ import fr.ubordeaux.ao.application.command.CommandMonitor;
 public abstract class Command {
     private List<CommandMonitor> monitorList;
 
-    public abstract void execute();
-    public abstract void addMonitor(CommandMonitor monitor);
-    public abstract void notifyMonitor();
+    public abstract void execute() {
+        executeCommand();
+        notifyMonitor();
+    }
+    
+    public void addMonitor(CommandMonitor monitor) {
+        if (monitor == null) {
+            throw new ReferenceManagementException("Monitor is null");
+        }
+        monitorList.add(monitor);
+    }
+    
+    private void notifyMonitor() {
+        for (CommandMonitor monitor : monitorList) {
+            monitor.commandIsExecuted();
+        }
+    }
+    
+    protected abstract void executeCommand();
 }
